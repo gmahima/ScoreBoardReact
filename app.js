39e95f992efb40ba6b9c7dc471f9298b2ae90635
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-07-20 21:14:05 +0530

    add props

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-function Header() {
+function Header(props) {
   return (
     <Header>
-      <h1>Scoreboard</h1>
-      <span className="stats">players: 1</span>
+      <h1>{props.title}</h1>
+      <span className="stats">players: {props.players}</span>
     </Header>
 
   )
 }
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className= "counter">
       <button className= "counter-action decrement">-</button>
-      <span className="counter-score">45</span>
+      <span className="counter-score">{props.score}</span>
       <button className= "counter-action decrement">-</button>)
     </div>
 }
-const Player = () => {
+const Player = (props) => {
  return (  <div className="player">
-    <span className="player-name">Guil</span>
-    <Counter />
+    <span className="player-name">{props.name}</span>
+    <Counter score={props.score}/>
   </div>)
 }
 const App = () => {
   return (
     <div className="scoreboard">
-      <Header title="scoreboard" players={1}/>
-      <Player />
+      <Header title="scoreboard" players={1} />
+      <Player score={400} name="the best player" />
     </div>
   )
 }
